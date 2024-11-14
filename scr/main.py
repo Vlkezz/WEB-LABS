c856b91d0f063ac7807020d9534701bd44aa0a2d
Vlkezz <makasin.2002@mail.ru>
2024-11-14 23:36:54 +0700

    Add multiply function

--- scr/main.py
+++ scr/main.py
@@ -3,3 +3,7 @@ print("Hello,world!")
 a = int(input("Введите первое число:"))
 b = int(input("Введите второе число:"))
 print(a+b)
+
+a = int(input("Введите первое число:"))
+b = int(input("Введите второе число:"))
+print(a*b)
